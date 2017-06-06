5eb8a2453e24d125c271d80229b35c9bcda91daa
davidmgudeman <davidmgudeman@gmail.com>
2017-06-06 08:18:59 -0700

    started on the angular2

--- angular-nodejs-project/routes/app.js
+++ angular-nodejs-project/routes/app.js
@@ -1,26 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc) {
-        if (err) {
-            return res.send('Error!');
-        }
-        res.render('node', {email: doc.email});
-    });
-});
-
-router.post('/', function (req, res, next) {
-    let email = req.body.email;
-    let user = new User({
-        firstName: 'Dave',
-        lastName: 'Gudeman',
-        password: 'super-secret',
-        email: email 
-    });
-    user.save();
-    res.redirect('/');
+        res.render('index');
 });
 
 module.exports = router;
