94ad0e4509d904e59b931c5f28f2a5f2eea22f78
davidmgudeman <davidmgudeman@gmail.com>
2017-06-06 17:01:36 -0700

    delete functionality works on the messages page

--- angular-nodejs-project/assets/app/messages/message.component.ts
+++ angular-nodejs-project/assets/app/messages/message.component.ts
@@ -4,6 +4,7 @@ import {
     Output,
      EventEmitter }         from '@angular/core';
 import { Message }          from './message.model';
+import { MessageService }          from './message.service';
 
 @Component ({
     selector: 'app-message',
@@ -26,8 +27,16 @@ import { Message }          from './message.model';
 export class MessageComponent {
     @Input() message: Message;
     @Output() editClicked = new EventEmitter<string>();
+
+    constructor(
+        private messageService: MessageService
+    ) {}
     
     onEdit() {
         this.editClicked.emit('A new value');
+    }
+
+    onDelete() {
+        this.messageService.deleteMessage(this.message);
     }
 }
