8b107761fbc325aa9ffac5dfb3dab695db375e96
davidmgudeman <davidmgudeman@gmail.com>
2017-06-07 11:21:07 -0700

    set up the messages route on the BE

--- angular-nodejs-project/app.js
+++ angular-nodejs-project/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var appRoutes = require('./routes/app');
+const messageRoutes = require('./routes/messages');
 
 var app = express();
 // connect database to server
@@ -31,7 +32,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
     next();
 });
-
+app.use('/message', messageRoutes);
 app.use('/', appRoutes);
 
 // catch 404 and forward to error handler
